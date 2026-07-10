2035febefdb5c4f5b44c7c6fdaf11921f2312342
Nilton Dionisio Guerra <niltondg.39@gmail.com>
2026-07-10 09:01:46 -0300

    initial commit

---
+++ estudos-spring-boot/src/main/java/com/example/estudos_spring_boot/EstudosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.estudos_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosSpringBootApplication.class, args);
+	}
+
+}
---
+++ estudos-spring-boot/src/test/java/com/example/estudos_spring_boot/EstudosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudos_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
